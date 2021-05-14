0d568c977e3e7a120141d6a5763f4c7234af90ff
Katja Hauser <katja_hauser@web.de>
2021-05-14 10:20:34 +0200

    Now using the openLTH framework, which is written using Pycharm instead of Tensorflow. Ported utils.prepare_ordered_dict_from_model to torch. Including the test. The rest of the files is messy at this point. Will be cleaned in the next commit, though.

--- noblankslate/src/utils.py
+++ noblankslate/src/utils.py
@@ -1,21 +1,85 @@
 from collections import OrderedDict
 
+import numpy as np
+
 
 def prepare_ordered_dict_from_model(model):
-    '''
-    tda.PerLayerCalculation requires the weights of a model to be presented as an OrderedDict. Due to changes from
-    Tensorflow 1.6 to 2.4, their utility function cannot be used under TF 2.4 out-of-the-box anymore. This function is
-    a substitute.
+    """
+    tda.PerLayerCalculation requires the weights of a model to be presented as an OrderedDict. Since the open_LTH
+    framework uses Pytorch and the neural persistence framework uses Tensorflow, we need a conversion.
 
-    This function works with the implicit assumption, that the weights are the first item that is returned when calling
-    layer.get_weights().
-
-    :param model: A TF model.
+    :param model: A Pytorch model.
     :return: An OrderedDict containing layer_name:numpy_weights tuples.
-    '''
+    """
 
     weights = OrderedDict()
-    for i, layer in enumerate(model.layers):
-        if len(layer.get_weights()) > 0:
-            weights.update({layer.name: layer.get_weights()[0]})
+    weight_keys = [key for key in model.state_dict().keys() if "weight" in key]
+    for key in weight_keys:
+        weights.update({key: model.state_dict()[key]})
     return weights
+
+
+    # todo in utils einpflegen
+    # todo tests schreiben
+def load_masked_network(path, num_layers):
+    """
+    Loads a masked network from a given path and returns an OrderedDict of the format {layer:masked weights} for
+    use with the neural persistence calculations.
+
+    Internally, both the network and the mask are loaded, then the mask is applied. This function implicitly
+    assumes that the format of the open lottery ticket hypothesis framework is used.
+
+    :param path: string
+    :return: OrderedDict
+    """
+
+    # todo checks: path is a directory not a file
+    # todo check: allow alternative naming for mask and weights folder and check if they exist
+
+    masked_weights = OrderedDict()
+
+    for i in range(num_layers):
+        final_weights = np.load(path+"final/layer{}.npy".format(str(i)))
+    # todo handling for missing masks in first training step (+ adaption of description/docu)
+        try:
+            mask = np.load(path+"masks/layer{}.npy".format(str(i)))
+            masked_weights["layer{}".format(str(i))] = final_weights * mask
+        except FileNotFoundError:
+            print("No mask found at path {}. Ignore this, if this is the "
+                  "first (i.e., unpruned) network. In the LTH framework, this is folder 0. Returning only the "
+                  "weights.".format(path + "masks/layer{}.npy".format(str(i))))
+            masked_weights["layer{}".format(str(i))] = final_weights
+
+    return masked_weights
+
+
+# def get_test_accuracy(path):
+#     """ todo docu :D im wesentlichen wollen wir die test accuracy bei der letzten iteration haben"""
+#     acc = -1
+#
+#     with open(path, "r") as file:
+#         last_line = file.readlines()[-1]
+#         acc = last_line.split(",")[-1]
+#
+#     return acc
+#
+# calc = PerLayerCalculation()
+#
+# # todo an passender stelle einpflegen, je nach fragestellung refactoren
+#
+# y = np.zeros((21, 5))
+#
+# for i in range(21):
+#     persistence = calc(load_masked_network("../initial_experiments/mnist_fc_data/trial1/{}/same_init/"
+#                                            .format(str(i)), 3))
+#     y[i, 0] = persistence["layer0"]["total_persistence_normalized"]
+#     y[i, 1] = persistence["layer1"]["total_persistence_normalized"]
+#     y[i, 2] = persistence["layer2"]["total_persistence_normalized"]
+#     y[i, 3] = persistence["global"]["accumulated_total_persistence_normalized"]
+#     y[i, 4] = get_test_accuracy("../initial_experiments/mnist_fc_data/trial1/{}/same_init/test.log".format(str(i)))
+#
+# x = np.arange(21)
+#
+# plt.plot(x, y)
+# plt.legend(["layer0", "layer1", "layer2", "global", "accuracy"])
+# plt.show()
--- noblankslate/tests/utils_tests.py
+++ noblankslate/tests/utils_tests.py
@@ -2,29 +2,51 @@ from collections import OrderedDict
 import unittest
 
 import numpy as np
-import numpy.testing as np_testing
 import tensorflow as tf
+import torch
+import torch.nn as nn
 
 import src.utils as utils
+
+
+# simplified openLTH model todo maybe fix paths later and use real openLTH model.
+class TestModel(nn.Module):
+
+    def __init__(self, plan, outputs=10):
+        super(TestModel, self).__init__()
+
+        layers = []
+        current_size = 4
+        for size in plan:
+            layers.append(nn.Linear(current_size, size))
+            current_size = size
+
+        self.fc_layers = nn.ModuleList(layers)
+        self.fc = nn.Linear(current_size, outputs)
+        for l in self.fc_layers:
+            nn.init.constant_(l.weight, -1)
+        nn.init.constant_(self.fc.weight, -1)
 
 
 class TestPrepareOrderedDictFromModel(unittest.TestCase):
     def test_on_vanilla_ff(self):
 
-        model = tf.keras.Sequential([
-            tf.keras.layers.Dense(2, kernel_initializer=tf.keras.initializers.constant(1.)),
-            tf.keras.layers.Dense(2, kernel_initializer=tf.keras.initializers.constant(1.))
-        ])
-
-        model(tf.ones((2, 2)))
-
-        true_weights = OrderedDict({"dense": np.ones((2, 2)), "dense_1": np.ones((2, 2))})
-
+        model = TestModel([2], outputs=2)
+        expected_keys = ['fc_layers.0.weight', 'fc.weight']
+        expected_fc_layers_0_weight = torch.tensor([[-1., -1., -1., -1.], [-1., -1., -1., -1.]])
+        expected_fc_weight = torch.tensor([[-1., -1.], [-1., -1.]])
         weights = utils.prepare_ordered_dict_from_model(model)
 
-        self.assertEqual(true_weights.keys(), weights.keys())
-        for k in true_weights.keys():
-            self.assertEqual(true_weights[k].tolist(), weights[k].tolist(), "Assertion caused by layer '{}'.".format(k))
+        self.assertEqual(expected_keys, list(weights.keys()))
+        np.testing.assert_array_equal(expected_fc_layers_0_weight, weights['fc_layers.0.weight'])
+        np.testing.assert_array_equal(expected_fc_weight, weights['fc.weight'])
+
+
+class TestLoadMaskedNetwork(unittest.TestCase):
+    def test_loading_on_vanilla_ff(self):
+        model = TestModel([2, 2])
+        torch.save(model, "./resources/test_load_masked_network")
+        print("asdf")
 
 
 if __name__ == '__main__':
